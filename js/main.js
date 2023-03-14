ace327ddfb34f6f047236721e854c6f1b2d4debb
MasterTomNL <tomvanderlek@gmail.com>
2023-03-14 10:11:30 +0100

    Delete Officers and Actions

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ class MasterTomNLDomainSheet5E extends dnd5e.applications.actor.ActorSheet5eChar
         this.actor.setFlag(mName, "powers", this.getPowers(html));
         this.actor.setFlag(mName, "relations", this.getRelations(html));
         this.actor.setFlag(mName, "actions", this.getActions(html));
+        this.actor.setFlag(mName, "officers", this.getOfficers(html));
+        console.log(this.actor);
     }
     
     /*
@@ -84,6 +86,35 @@ class MasterTomNLDomainSheet5E extends dnd5e.applications.actor.ActorSheet5eChar
         return actions;
     }
     
+    /*
+     * get all officers from our character sheet
+     */
+    getOfficers(html) {
+        let officers = [];
+        let ids = $(html).find('.delete-officer');
+        let images = $(html).find('[name="'+preFlix+'officer.img[]"]');
+        let titles = $(html).find('[name="'+preFlix+'officer.title[]"]');
+        let names = $(html).find('[name="'+preFlix+'officer.name[]"]');
+        let bonuses = $(html).find('[name="'+preFlix+'officer.bonus[]"]');
+        
+        for (var i=0; i<images.length; i++) {
+            officers.push({
+                "id": $(ids[i]).attr('data-officer-id'),
+                "img": $(images[i]).attr('src'),
+                "title": $(titles[i]).val(),
+                "name": $(names[i]).val(),
+                "bonus": $(bonuses[i]).val()
+            });
+        }
+        return officers;
+    }
+    
+    getUuid() {
+        return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
+            (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+        );
+    }
+    
     static get defaultOptions() {
         const options = super.defaultOptions;
         // sheet window options
@@ -116,11 +147,37 @@ class MasterTomNLDomainSheet5E extends dnd5e.applications.actor.ActorSheet5eChar
             .on("click", (event) => {
                 this.addRelation(html);
             });
+        // when you click on the delete button
+        $(html)
+            .find('a.delete-relation')
+            .on("click", (event) => {
+                this.deleteRelation(html, event.target.getAttribute("data-relation-id"));
+            });
+        
         // when you click on the + we will add a (blank) action
         $(html)
             .find('#add-action')
             .on("click", (event) => {
                 this.addAction(html);
+            });
+        // when you click on the delete button
+        $(html)
+            .find('a.delete-action')
+            .on("click", (event) => {
+                this.deleteAction(html, event.target.getAttribute("data-action-id"));
+            });
+
+        // when you click on the + we will add a (blank) officer
+        $(html)
+            .find('#add-officer')
+            .on("click", (event) => {
+                this.addOfficer(html);
+            });
+        // when you click on the delete button
+        $(html)
+            .find('a.delete-officer')
+            .on("click", (event) => {
+                this.deleteOfficer(html, event.target.getAttribute("data-officer-id"));
             });
 
         return true;
@@ -134,8 +191,21 @@ class MasterTomNLDomainSheet5E extends dnd5e.applications.actor.ActorSheet5eChar
         // get existing relations
         let relations = this.getRelations(html);
         // add a (blank) relation
-        relations.push({"name": "", "standing": "", "size": ""});
+        relations.push({"id": this.getUuid(), "name": "", "standing": "", "size": ""});
         // save it to FLAGS
+        this.actor.setFlag(mName, "relations", relations);
+        return ;
+    }
+    
+    deleteRelation(html, id) {
+        console.log("MasterTomNL-Domain-Sheet-5e | Delete a relation.");
+        let relations = this.getRelations(html);
+        for (var i=0; i<relations.length; i++) {
+            if (relations[i].id == id) {
+                relations.splice(i, 1);
+                break;
+            }
+        }
         this.actor.setFlag(mName, "relations", relations);
         return ;
     }
@@ -148,9 +218,49 @@ class MasterTomNLDomainSheet5E extends dnd5e.applications.actor.ActorSheet5eChar
         // get existing actions
         let actions = this.getActions(html);
         // add a (blank) relation
-        actions.unshift({"title": "", "roll": "", "result": ""});
+        actions.unshift({"id": this.getUuid(), "title": "", "roll": "", "result": ""});
         // save it to FLAGS
         this.actor.setFlag(mName, "actions", actions);
+        return ;
+    }
+    
+    deleteAction(html, id) {
+        console.log("MasterTomNL-Domain-Sheet-5e | Delete an action.");
+        let actions = this.getActions(html);
+        for (var i=0; i<actions.length; i++) {
+            if (actions[i].id == id) {
+                actions.splice(i, 1);
+                break;
+            }
+        }
+        this.actor.setFlag(mName, "actions", actions);
+        return ;
+    }
+    
+    /*
+     * function to add an officer
+     */
+    addOfficer(html) {
+        console.log("MasterTomNL-Domain-Sheet-5e | Add an officer.");
+        // get existing officers
+        let officers = this.getOfficers(html);
+        // add a (blank) officer
+        officers.push({"id": this.getUuid(), "img": "", "name": "", "title": "", "bonus": ""});
+        // save it to FLAGS
+        this.actor.setFlag(mName, "officers", officers);
+        return ;
+    }
+    
+    deleteOfficer(html, id) {
+        console.log("MasterTomNL-Domain-Sheet-5e | Delete an officer.");
+        let officers = this.getOfficers(html);
+        for (var i=0; i<officers.length; i++) {
+            if (officers[i].id == id) {
+                officers.splice(i, 1);
+                break;
+            }
+        }
+        this.actor.setFlag(mName, "officers", officers);
         return ;
     }
 }
